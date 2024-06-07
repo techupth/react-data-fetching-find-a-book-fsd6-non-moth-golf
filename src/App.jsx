b9492f32572a20a1f71305deacb8d29cf2493884
KeeratiNon <keeratinon3@gmail.com>
2024-06-07 14:52:11 +0700

    get-data-from-google-by-input-book-name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [text, setText] = useState("")
+  const [book, setBook] = useState([])
+  const getBook = async () => {
+    try {
+      const result = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}`)
+      console.log(result.data.items)
+      setBook(result.data.items)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect(()=>{
+    getBook()
+  },[text])
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" placeholder="Enter a book" value={text} onChange={(e)=>{setText(e.target.value)}}></input>
+      {
+        book.map((data)=>{
+          return (
+            <ul>
+              <li>{data.volumeInfo.title}</li>
+            </ul>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
