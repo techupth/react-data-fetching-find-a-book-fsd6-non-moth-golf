87693c5af65882b3bb1c02aef13f48b753d9c347
KeeratiNon <keeratinon3@gmail.com>
2024-06-07 15:11:42 +0700

    add-condition-when-input-is-empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,46 @@
 import "./App.css";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [text, setText] = useState("")
-  const [book, setBook] = useState([])
+  const [text, setText] = useState("");
+  const [book, setBook] = useState([]);
   const getBook = async () => {
     try {
-      const result = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}`)
-      console.log(result.data.items)
-      setBook(result.data.items)
+      if (text !== "") {
+        const result = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${text}`
+        );
+        console.log(result.data.items);
+        setBook(result.data.items);
+      }
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-  useEffect(()=>{
-    getBook()
-  },[text])
+  };
+  useEffect(() => {
+    getBook();
+  }, [text]);
   return (
     <div className="App">
       <h1>Find a Book</h1>
-      <input type="text" placeholder="Enter a book" value={text} onChange={(e)=>{setText(e.target.value)}}></input>
-      {
-        book.map((data)=>{
-          return (
-            <ul>
-              <li>{data.volumeInfo.title}</li>
-            </ul>
-          )
-        })
-      }
+      <input
+        type="text"
+        placeholder="Enter a book"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      ></input>
+      <ul>
+        {text
+          ? book.map((data, index) => {
+              return <li key={index}>{data.volumeInfo.title}</li>;
+            })
+          : null}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
