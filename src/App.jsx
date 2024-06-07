cecb7951efbdc30b227b6c616cea0cbb6c000519
Moth <kampanee.moth@gmail.com>
2024-06-07 16:07:36 +0700

    feat: create input search book to google api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [isFind, setIsFind] = useState("");
+  const [displayList, setDisplayList] = useState([]);
+
+  const baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+
+  async function searchBook(event) {
+    setIsFind(event.target.value);
+  }
+
+  async function findBook() {
+    try {
+      const response = await axios.get(`${baseUrl}${isFind}`);
+      setDisplayList(response.data.items);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  useEffect(() => {
+    findBook();
+  }, [isFind]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input type="text" onChange={searchBook} />
+      {isFind
+        ? displayList.map((book, index) => (
+            <ul>
+              <li key={index}>{book.volumeInfo.title}</li>
+            </ul>
+          ))
+        : null}
+    </div>
+  );
 }
 
 export default App;
